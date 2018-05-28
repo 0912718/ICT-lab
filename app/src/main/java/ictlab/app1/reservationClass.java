1c2016cb150b88e55e5252b28327862c3f040327
Zina <Zina_1905@hotmail.com>
2018-05-28 13:16:35 +0200

    Revert "qr code and settings added"
    This reverts commit db0f7b418c7f144733ef30457db25df9b0fd145a.

--- app/src/main/java/ictlab/app1/reservationClass.java
+++ app/src/main/java/ictlab/app1/reservationClass.java
@@ -1,16 +1,8 @@
 package ictlab.app1;
 
-import android.app.Activity;
-import android.os.Bundle;
-
 /**
  * Created by edgar on 4-3-2018.
  */
 
-public class reservationClass extends Activity {
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.reservations);
-    }
+public class reservationClass {
 }
